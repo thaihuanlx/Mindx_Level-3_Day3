9c89e0d4b8be36a589306f515238e574a1d42096
thaihuanlx <thaihuanlx@gmail.com>
2024-03-05 09:38:09 +0700

    first commit

---
+++ handle.js
@@ -0,0 +1,83 @@
+let users = [];
+let posts = [];
+
+// Hàm sinh id ngẫu nhiên
+const generateId = () => {
+  const id = "US" + Math.floor(Math.random() * 10000);
+  return id;
+};
+
+// Hàm tạo user
+export const createUser = (userName) => {
+  const id = generateId();
+  const user = { id, userName };
+  users.push(user);
+  return user;
+};
+
+// Hàm tạo bài post
+export const createPost = (userId, content) => {
+  const id = generateId();
+  const post = { id, userId, content, comments: [] };
+  posts.push(post);
+  return post;
+};
+
+// Hàm chỉnh sửa bài post
+export const editPost = (postId, userId, content) => {
+  const post = posts.find((post) => post.id === postId);
+  if (post && post.userId === userId) {
+    post.content = content;
+    return post;
+  } else {
+    return null;
+  }
+};
+
+// Hàm comment vào bài post
+export const createComment = (postId, userId, content) => {
+  const post = posts.find((post) => post.id === postId);
+  if (post) {
+    const comment = { id: generateId(), userId, content };
+    post.comments.push(comment);
+    return comment;
+  } else {
+    return null;
+  }
+};
+
+// Hàm chỉnh sửa comment
+export const editComment = (commentId, userId, content) => {
+  for (let post of posts) {
+    const comment = post.comments.find((comment) => comment.id === commentId);
+    if (comment && comment.userId === userId) {
+      comment.content = content;
+      return comment;
+    }
+  }
+  return null;
+};
+
+// Hàm lấy tất cả comment của một bài post
+export const getAllComments = (postId) => {
+  const post = posts.find((post) => post.id === postId);
+  if (post) {
+    return post.comments;
+  } else {
+    return null;
+  }
+};
+
+// Hàm lấy tất cả các bài post và 3 comment đầu của tất cả user
+export const getAllPosts = () => {
+  return posts.map((post) => ({
+    ...post,
+    comments: post.comments.slice(0, 3),
+  }));
+};
+
+// Hàm lấy một bài post và tất cả comment của bài post đó thông qua postId
+export const getPostWithComments = (postId) => {
+  const post = posts.find((post) => post.id === postId);
+  return post ? post : null;
+};
---
+++ server.js
@@ -0,0 +1,78 @@
+import express from "express";
+import {
+  createUser,
+  createPost,
+  editPost,
+  createComment,
+  editComment,
+  getAllComments,
+  getAllPosts,
+  getPostWithComments,
+} from "./handle.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Đăng ký user
+app.post("/users", (req, res) => {
+  const { userName } = req.body;
+  const user = createUser(userName);
+  res.json(user);
+});
+
+// Tạo bài post
+app.post("/posts", (req, res) => {
+  const { userId, content } = req.body;
+  const post = createPost(userId, content);
+  res.json(post);
+});
+
+// Chỉnh sửa bài post
+app.put("/posts/:postId", (req, res) => {
+  const { postId } = req.params;
+  const { userId, content } = req.body;
+  const post = editPost(postId, userId, content);
+  res.json(post);
+});
+
+// Comment vào bài post
+app.post("/posts/:postId/comments", (req, res) => {
+  const { postId } = req.params;
+  const { userId, content } = req.body;
+  const comment = createComment(postId, userId, content);
+  res.json(comment);
+});
+
+// Chỉnh sửa comment
+app.put("/comments/:commentId", (req, res) => {
+  const { commentId } = req.params;
+  const { userId, content } = req.body;
+  const comment = editComment(commentId, userId, content);
+  res.json(comment);
+});
+
+// Lấy tất cả comment của một bài post
+app.get("/posts/:postId/comments", (req, res) => {
+  const { postId } = req.params;
+  const comments = getAllComments(postId);
+  res.json(comments);
+});
+
+// Lấy tất cả các bài post và 3 comment đầu của tất cả user
+app.get("/posts", (req, res) => {
+  const posts = getAllPosts();
+  res.json(posts);
+});
+
+// Lấy một bài post và tất cả comment của bài post đó thông qua postId
+app.get("/posts/:postId", (req, res) => {
+  const { postId } = req.params;
+  const postWithComments = getPostWithComments(postId);
+  res.json(postWithComments);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
